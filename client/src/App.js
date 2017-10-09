92848f73490e2cfe99b4b0c8fa9fee8c18e569a0
Shaina Karasin <skarasin39@gmail.com>
2017-10-09 10:27:07 -0400

    Updates to App

--- client/src/App.js
+++ client/src/App.js
@@ -4,29 +4,71 @@ import './App.css';
 import Auth from 'j-toker';
 
 class App extends Component {
-  TestAPI() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      logInResult: ''
+    };
+  }
+
+  configureAPI() {
     Auth.configure({
       apiUrl: '/api'
     });
+  };
 
-
+  // email: 'foo@bar.com',
+  // password: 'password'
+  logInAPI() {
     Auth.emailSignIn({
-      email: 'foo@bar.com',
-      password: 'password'
+      email: this.state.email,
+      password: this.state.password
+    }).then(function(response) {
+      console.log(response);
+      if (response.data) {
+        this.setState({
+          result: 'Success! You are logged in as ' + response.data.email
+        });
+      }
+    }.bind(this))
+      .fail( function() {
+        console.log('caught an error!');
+        this.setState({
+          result: "Sorry, you're login attempt failed"
+        });
+      }.bind(this));
+  };
+
+  updateEmail(evt) {
+    this.setState({
+      email: evt.target.value
+    });
+  };
+  updatePassword(evt) {
+    this.setState({
+      password: evt.target.value
     });
   };
 
+
   render() {
-    this.TestAPI();
+    this.configureAPI();
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Login Form">
+          <p>Log In to To-Do Application</p>
+          <p>Email: <input value={this.state.email} onChange={evt => this.updateEmail(evt)}/></p>
+          <p>Password: <input value={this.state.password} onChange={evt => this.updatePassword(evt)}/></p>
+          <p>Testing: {this.state.email} {this.state.password}</p>
+          <button onClick={() => this.logInAPI()}>Log In</button>
+          <p>{this.state.result}</p>
+        </div>
       </div>
     );
   }
